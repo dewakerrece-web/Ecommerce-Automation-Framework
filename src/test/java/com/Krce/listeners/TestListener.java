dc708befeed4cf31ea4c5aca6731dc0d8d830f41
dewakerrece-web <dewaker.r.ece@gmail.com>
2026-05-29 15:45:29 +0530

    feat(shanawaz): Add ExtentReports, TestListener and testng.xml suite configuration

---
+++ src/test/java/com/Krce/listeners/TestListener.java
@@ -0,0 +1,50 @@
+package com.Krce.listeners;
+
+import com.Krce.utilities.ExtentReportManager;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class TestListener implements ITestListener {
+
+    private static ExtentReports extent =
+            ExtentReportManager.getReportInstance();
+
+    private static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        ExtentTest extentTest = extent.createTest(
+                result.getMethod().getMethodName()
+        );
+        test.set(extentTest);
+        test.get().log(Status.INFO, "Test Started: "
+                + result.getMethod().getMethodName());
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        test.get().log(Status.PASS, "Test Passed: "
+                + result.getMethod().getMethodName());
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        test.get().log(Status.FAIL, "Test Failed: "
+                + result.getMethod().getMethodName());
+        test.get().log(Status.FAIL, result.getThrowable());
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        test.get().log(Status.SKIP, "Test Skipped: "
+                + result.getMethod().getMethodName());
+    }
+
+    @Override
+    public void onFinish(org.testng.ITestContext context) {
+        extent.flush();
+    }
+}
